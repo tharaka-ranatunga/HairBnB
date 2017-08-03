2c2796938bac7bb9063f88c4e01eea73bf3db3cc
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-08-03 11:08:33 +0530

    testing title of home page

--- app/test/sample.js
+++ app/test/sample.js
@@ -1,31 +1,10 @@
 /**
  * Created by tharaka_ra on 8/2/2017.
  */
-var casper = require('casper').create();
-
-
-casper.start('http://localhost:63342/HairBnB/app/views/#!');
-
-casper.wait(3000, function(){
-    this.echo(this.getTitle());
-});
-
-casper.then(function(){
-    casper.exit();
-});
-
-
-casper.run();
-
-
-//
-// phantom.casperPath = '../vendor/casperjs-1.1.4-1/bin/casperjs';
-// phantom.injectJs('../vendor/casperjs-1.1.4-1/bin/bootstrap.js');
-// phantom.casperTest = true;
-// // var utils = require('untils');
 // var casper = require('casper').create();
 //
-// casper.start('https://google.com');
+//
+// casper.start('http://localhost:63342/HairBnB/app/views/#!');
 //
 // casper.wait(3000, function(){
 //     this.echo(this.getTitle());
@@ -34,5 +13,52 @@ casper.run();
 // casper.then(function(){
 //     casper.exit();
 // });
-//
+
+// casper.test.begin('Hello, Test!', 1, function(test) {
+//     test.assert(true);
+//     test.done();
+// });
+
 // casper.run();
+
+
+//
+// var casper = require('casper').create();
+// var links;
+//
+// function getLinks() {
+// // Scrape the links from top-right nav of the website
+//     var links = document.querySelectorAll('ul.navigation li a');
+//     return Array.prototype.map.call(links, function (e) {
+//         return e.getAttribute('href')
+//     });
+// }
+//
+// // Opens casperjs homepage
+// casper.start('http://localhost:63342/HairBnB/app/views/#!');
+//
+// casper.then(function () {
+//     links = this.evaluate(getLinks);
+// });
+//
+// casper.run(function () {
+//     for(var i in links) {
+//         console.log(links[i]);
+//     }
+// });
+
+casper.test.begin('Testing title', 1, function(test){
+    casper.start('http://localhost:63342/HairBnB/app/views/#!');
+
+//     casper.wait(3000, function(){
+//     this.echo(this.getTitle());
+// });
+
+    casper.then(function(){
+        test.assertTitle('Hairbnb', 'Application has correct title');
+    });
+
+    casper.run(function(){
+        test.done();
+    })
+});
