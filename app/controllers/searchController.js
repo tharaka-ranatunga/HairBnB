6c09443b003351c26cdcce5d35bfd2cb20e0c9ac
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-08-04 12:53:06 +0530

    filter results view

--- app/controllers/searchController.js
+++ app/controllers/searchController.js
@@ -4,21 +4,56 @@
 myApp.controller('SearchController',
     ['$scope','$http','AuthService','$location',
         function ($scope,$http,AuthService,$location) {
-            $scope.job_types = ["Select All", "Stylist", "Educator", "Assistant", ];
-            $scope.skill_types = ["Select All","Barber", "Freehand", "Razor", "Shaving", "Scissor over comb",
-                "Texturing", "Clipper work", "Classics", "Dry cutting", "Wig cutting", "Long hair", "Short hair", "Makeup", "Hair coloring"];
-            $scope.location_list = ["Select All", "Sydney", "Melbourne", "Adelaide","Victoria", "Queensland" ];
-            $scope.selectedName = $scope.job_types[0];
-            $scope.selectedSkillName = $scope.skill_types[0];
+
+            $scope.job_types = [{name: "Select All", value: false},{name: "Stylist", value: false}, {name: "Educator", value: false},
+                {name: "Assistant", value: false}];
+
+            $scope.isLoading = false;
+            $scope.search_results = [];
+
+            $scope.skill_types = [{name: "Select All", value: false},{name: "Barber",value: false}, {name: "Freeehand",value: false}, {name: "Razor",value: false},
+                {name: "Shaving",value: false}, {name: "Scissor over comb",value: false}, {name: "Texturing",value: false},
+                {name: "Clipper work",value: false}, {name: "Classics",value: false}, {name: "Dry cutting",value: false},
+                {name: "Wig cutting",value: false}, {name: "Long hair",value: false}, {name: "Short hair",value: false},
+                {name: "Makeup",value: false}, {name: "Hair coloring",value: false}, ];
+
+            $scope.selectedName = $scope.job_types[0].name;
+            $scope.selectedSkillName = $scope.skill_types[0].name;
             $scope.empty_results = false;
 
+            // $scope.job_types = ["Select All","Stylist", "Educator", "Assistant" ];
+            // $scope.skill_types = ["Select All","Barber", "Freehand", "Razor", "Shaving", "Scissor over comb",
+            //     "Texturing", "Clipper work", "Classics", "Dry cutting", "Wig cutting", "Long hair", "Short hair", "Makeup", "Hair coloring"];
+            $scope.location_list = [{name: "Melbourne",value: false}, {name: "Sydney",value: false}, {name: "Adelaide", value: false}, {name: "Victoria",value: false}, {name: "Queensland", value: false} ];
+            // $scope.selectedName = $scope.job_types[0];
+            // $scope.selectedSkillName = $scope.skill_types[0];
+            // $scope.empty_results = false;
+
             $scope.beginSearch = function (skill, jobtype) {
-                var jobtypes, skilltype;
-                if($scope.job_types.indexOf(jobtype)===0){jobtypes = 'all'}
-                else{jobtypes = ($scope.job_types.indexOf(jobtype))}
-                if($scope.skill_types.indexOf(skill)===0){skilltype = 'all'}
-                else{skilltype = ($scope.skill_types.indexOf(skill))}
+                var jobtypes, skilltype, index;
+                for(index=0; index<$scope.job_types.length; index++){
+                    if($scope.job_types[index].name===jobtype){
+                        if(index===0){jobtypes = 'all';}else{jobtypes=(index+1);}
+                        break;
+                    }
+                }
+                for(index=0; index<$scope.skill_types.length; index++){
+                    if($scope.skill_types[index].name===skill){
+                        if(index===0){skilltype = 'all';}else{skilltype=(index+1);}
+                        break;
+                    }
+                }
+                console.log('type: ' + jobtypes + ', skill: ' + skilltype );
                 $location.path('/search').search({jobtype: jobtypes, skilltype: skilltype});
+
+
+                // var jobtypes, skilltype;
+                // console.log(skill + '    ' + jobtype);
+                // if($scope.job_types.indexOf(jobtype)===0){jobtypes = 'all'}
+                // else{jobtypes = ($scope.job_types.indexOf(jobtype))}
+                // if($scope.skill_types.indexOf(skill)===0){skilltype = 'all'}
+                // else{skilltype = ($scope.skill_types.indexOf(skill))}
+
             };
 
             $scope.viewProfile = function (id) {
