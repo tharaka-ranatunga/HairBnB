929bd8036fbc96d7cf68dbd7e1401ec4bc913bc5
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-08-10 12:42:48 +0530

    Profile edit done

--- app/controllers/editController.js
+++ app/controllers/editController.js
@@ -9,7 +9,7 @@ myApp.controller('EditController',
             $scope.job_types = [{name: "Stylist", value: false}, {name: "Educator", value: false},
                 {name: "Assistant", value: false}];
 
-            $scope.skill_types = [{name: "Freeehand",value: false}, {name: "Razor",value: false},
+            $scope.skill_types = [{name: "Barber",value: false}, {name: "Freeehand",value: false}, {name: "Razor",value: false},
                 {name: "Shaving",value: false}, {name: "Scissor over comb",value: false}, {name: "Texturing",value: false},
                 {name: "Clipper work",value: false}, {name: "Classics",value: false}, {name: "Dry cutting",value: false},
                 {name: "Wig cutting",value: false}, {name: "Long hair",value: false}, {name: "Short hair",value: false},
@@ -17,85 +17,16 @@ myApp.controller('EditController',
 
             $scope.submitted = false;       //Error showing only after submit button clicked
             $scope.isComplete = false;      //Check all the fields are filled
-            $scope.repass = false;          //Check password match only after user retype password
-            $scope.des_error = false;       //Check account description is empty or not
 
             //-------------------------Show Success/Danger alert boxes
             $scope.success = false;
             $scope.danger = false;
             $scope.message = '';
-            //-------------------------Show Sections
-            $scope.password_reset_section = false;
-            $scope.profile_reset_section = true;
-            $scope.skills_jobtype_section = false;
-            $scope.payment_section = false;
 
-            //-------------------------Password Character Error
-            $scope.new_password_chr_error = false;
 
-            //Description character count
-            $scope.desLength= 0;
-            $scope.characters = 1000;
-            $scope.chrLengthError = false;
 
-            $scope.resetSubmitted = function () {
-                $scope.submitted = false;
-                try {
-                    if ($scope.newPassword.length !== 'undefined') {
-                        if ($scope.newPassword.length === 0) {
-                            $scope.new_password_chr_error = false;
-                        } else if ($scope.newPassword.length >= 8) {
-                            $scope.new_password_chr_error = false;
-                        } else {
-                            $scope.new_password_chr_error = true;
-                        }
-                    }
-                } catch (err) {
-                    $scope.new_password_chr_error = false;
-                }
-            };
-
-            $scope.setRePass = function () {
-                if($scope.newPassword==="" || typeof $scope.newPassword==="undefined"){$scope.repass = false;}
-                else {$scope.repass = true;}
-            };
-
-            $scope.validate = function () {
-                if (typeof $scope.password === "undefined" || $scope.password === "") {
-                    $scope.isComplete = false;return;
-                }
-                if (typeof $scope.newPassword === "undefined" || $scope.newPassword === "") {
-                    $scope.isComplete = false;return;
-                }
-                if($scope.newPassword !== $scope.reNewPassword){$scope.isComplete = false;return;}
-                $scope.isComplete = true;
-            };
-
-            $scope.resetAlertBoxes = function () {
-                $scope.success = false;
-                $scope.danger = false;
-                $scope.message = '';
-            };
-
-            $scope.profile = function () {
-                $scope.profile_reset_section = true;
-                $scope.password_reset_section = false;
-                $scope.skills_jobtype_section = false;
-                $scope.payment_section = false;
-            };
-
-            $scope.payment = function () {
-                $scope.profile_reset_section = false;
-                $scope.password_reset_section = false;
-                $scope.skills_jobtype_section = false;
-                $scope.payment_section = true;
-            };
 
             $scope.skills_jobtypes = function () {
-                $scope.profile_reset_section = false;
-                $scope.password_reset_section = false;
-                $scope.skills_jobtype_section = true;
-                $scope.payment_section = false;
 
                 var user = AuthService.getUser();
                 if(user){
@@ -133,112 +64,61 @@ myApp.controller('EditController',
 
             };
 
-            $scope.password_reset = function () {
-                $scope.password_reset_section = true;
-                $scope.profile_reset_section = false;
-                $scope.skills_jobtype_section = false;
-                $scope.payment_section = false;
-            };
 
-            $scope.accDescription = false;
-            $scope.accSkills = false;
+            // $scope.onInit = function () {
+            //     $scope.success = false;
+            //     $scope.danger = false;
+            //     $http({
+            //         method: "GET",
+            //         url: "http://localhost:3000/profile/getEditProfile"
+            //     }).then(function (resData){
+            //         //Profile Found
+            //         $scope.accDescription = true;
+            //         $scope.accSkills = true;
+            //
+            //         $http({
+            //             method: "GET",
+            //             url: "http://localhost:3000/profile/getEditProfile"
+            //         }).then(function (resData) {
+            //             //Profile Found
+            //
+            //             $scope.characters = (1000-$scope.description.length);
+            //         },function (error) {
+            //             console.log('Sever error occurred, redirecting to home');
+            //             $location.path('/');
+            //         });
+            //     },function (error){
+            //         if(error.status===404){
+            //             //No Profile found
+            //             $scope.accDescription = false;
+            //             $scope.accSkills = false;
+            //         }else {
+            //             console.log('Sever connection error occurred, redirecting to home');
+            //             $location.path('/');
+            //         }
+            //     });
+            // };
 
-            $scope.onInit = function () {
-                $scope.success = false;
-                $scope.danger = false;
-                $http({
-                    method: "GET",
-                    url: "http://localhost:3000/profile/getEditProfile"
-                }).then(function (resData){
-                    //Profile Found
-                    $scope.accDescription = true;
-                    $scope.accSkills = true;
+            // $scope.saveProfile = function () {
+            //     $scope.des_error = false;
+            //     if($scope.description.length===0 || typeof $scope.description==='undefined'){
+            //         $scope.des_error = true;
+            //     }
+            //     if(!($scope.des_error) && !($scope.chrLengthError)) {
+            //         $http({
+            //             method: "POST",
+            //             url: "http://localhost:3000/profile/updateProfile",
+            //             data: {description: $scope.description}
+            //         }).then(function (resData) {
+            //             $scope.success = true;
+            //             $scope.message = 'Profile updated successfully!';
+            //         }, function (error) {
+            //             console.log('Error: ' + error);
+            //         });
+            //     }
+            // };
 
-                    $http({
-                        method: "GET",
-                        url: "http://localhost:3000/profile/getEditProfile"
-                    }).then(function (resData) {
-                        //Profile Found
 
-                        $scope.characters = (1000-$scope.description.length);
-                    },function (error) {
-                        console.log('Sever error occurred, redirecting to home');
-                        $location.path('/');
-                    });
-                },function (error){
-                    if(error.status===404){
-                        //No Profile found
-                        $scope.accDescription = false;
-                        $scope.accSkills = false;
-                    }else {
-                        console.log('Sever connection error occurred, redirecting to home');
-                        $location.path('/');
-                    }
-                });
-            };
-
-            $scope.saveProfile = function () {
-                $scope.des_error = false;
-                if($scope.description.length===0 || typeof $scope.description==='undefined'){
-                    $scope.des_error = true;
-                }
-                if(!($scope.des_error) && !($scope.chrLengthError)) {
-                    $http({
-                        method: "POST",
-                        url: "http://localhost:3000/profile/updateProfile",
-                        data: {description: $scope.description}
-                    }).then(function (resData) {
-                        $scope.success = true;
-                        $scope.message = 'Profile updated successfully!';
-                    }, function (error) {
-                        console.log('Error: ' + error);
-                    });
-                }
-            };
-
-            $scope.changePassword = function () {
-                $scope.success = false;
-                $scope.danger = false;
-                $scope.submitted = true;
-                if(!$scope.isComplete){return;}
-                if($scope.new_password_chr_error){return;}
-                if($scope.password===$scope.newPassword){
-                    $scope.danger = true;
-                    $scope.message = "New password must be different from old password";
-                    return;
-                }
-                $http({
-                    method: "POST",
-                    url: "http://localhost:3000/profile/changePassword",
-                    data: {curPassword: $scope.password, newPassword: $scope.newPassword}
-                }).then(function (resData){
-                    console.log(resData);
-                    if(resData.status===200){
-                        $scope.success = true;
-                        $scope.message = "Your password changed successfully!";
-                        $scope.danger = false;
-                        //Reset all password fields
-                        $scope.submitted = false;
-                        $scope.password = "";
-                        $scope.newPassword = "";
-                        $scope.reNewPassword = "";
-
-                    }
-                },function (error){
-                    if(error.status===401){
-                        $scope.success = false;
-                        $scope.danger = true;
-                        $scope.message = "Current password is invalid!";
-                    }else {
-                        $scope.success = false;
-                        $scope.danger = true;
-                        $scope.message = "Server Error";
-                    }
-                });
-            };
-
-            $scope.job_type_err = false;
-            $scope.skill_error = false;
             $scope.saveSkillsJobTypes = function () {
                 var i;
                 $scope.job_type_err = true;
@@ -266,8 +146,8 @@ myApp.controller('EditController',
                     }
                     $http({
                         method: "POST",
-                        url: "http://localhost:3000/profile/updateProfileSkillsTypes",
-                        data: {job_types_arr: job_types_arr, skill_types_arr: skill_types_arr}
+                        url: "http://localhost:3000/profile/updateProfile",
+                        data: {job_types_arr: job_types_arr, skill_types_arr: skill_types_arr, description: $scope.description}
                     }).then(function (resData){
                         // console.log(resData);
                         if(resData.status===200){
@@ -283,17 +163,6 @@ myApp.controller('EditController',
                 }
             };
 
-            $scope.updateDesLength = function () {
-                if($scope.description.length > 1000){
-                    $scope.characters = $scope.description.length - 1000;
-                    $scope.chrLengthError = true;
-                }else{
-                    $scope.characters = 1000 - $scope.description.length;
-                    $scope.chrLengthError = false;
-                }
-            };
-
-            $scope.reset_des_error = function () {$scope.des_error = false;};
         }
     ]);
 
