44c5606a3c751cda4c03bf61cbbac33d20ddc982
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-08-11 14:33:19 +0530

    more on functional testing

--- todo.spec.js
+++ todo.spec.js
@@ -2,14 +2,46 @@
  * Created by tharaka_ra on 8/10/2017.
  */
 
-describe('Testing Hairbnb', function() {
+describe('Hairbnb Testing', function() {
     var mail = 'tharaka.kamal@gmail.com';
     var password = 'Aa1111';
+    var firstn = "Tharaka";
+    var lastn = "Ranatunga";
+    var emailn = "tharaka.kamal@gmail.com";
+    var passwordn = "Aa1111";
+    var confirmn = "Aa1111";
     var EC = protractor.ExpectedConditions;
-    it('should have the title', function() {
+
+    it('should check users already exist', function() {
         browser.get('http://localhost:63342/HairBnB/app/views/#!/');
-        expect(browser.getTitle()).toEqual('Hairbnb');
+        e = element(by.id('signup_button'));
+        e.click();
+        firstk = element(by.id('first'));
+        for(var i = 0; i < firstn.length; i++){
+            firstk.sendKeys(firstn.charAt(i));
+        }
+        lastk = element(by.id('last'));
+        for(var i = 0; i <lastn.length; i++){
+            lastk.sendKeys(lastn.charAt(i));
+        }
+        emailk = element(by.id('email'));
+        for(var i = 0; i < emailn.length; i++){
+            emailk.sendKeys(emailn.charAt(i));
+        }
+        passwordk = element(by.id('password'));
+        for(var i = 0; i < passwordn.length; i++){
+            passwordk.sendKeys(passwordn.charAt(i));
+        }
+        confirmk = element(by.id('confirm'));
+        for(var i = 0; i < confirmn.length; i++){
+            confirmk.sendKeys(confirmn.charAt(i));
+        }
+        element(by.id("sign")).click();
+        message = element(by.id('signup_message')).getText();
+        expect(message).toEqual('This email already registered');
     });
+
+
     it('should sign in the user', function() {
         browser.get('http://localhost:63342/HairBnB/app/views/#!/');
         // var signin_button = element(by.css('.modal.fade #signin'))
@@ -33,6 +65,7 @@ describe('Testing Hairbnb', function() {
         // element(by.id('upassword')).sendKeys('Aa1111')
         // expect(browser.getTitle()).toEqual('Hairbnb');
     });
+
     it('should go to user profile', function(){
         d = element(by.id('user_dropdown'));
         browser.wait(EC.visibilityOf(d), 5000);
@@ -49,6 +82,13 @@ describe('Testing Hairbnb', function() {
         expect(email).toEqual('tharaka.kamal@gmail.com')
     });
 
+    it('should show search results', function(){
+        f = element(by.id('brand'));
+        browser.wait(EC.visibilityOf(f), 5000);
+        f.click();
+    });
+
 
 
 });
+
